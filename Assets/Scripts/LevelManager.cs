f5c0e0c5fe4fc056739d35ad2d89549a2628bcf7
Syrenzo <46717180+Syrenzo@users.noreply.github.com>
2019-01-29 15:42:39 +0000

    Test

--- Assets/Scripts/LevelManager.cs
+++ Assets/Scripts/LevelManager.cs
@@ -15,7 +15,7 @@ public class LevelManager : MonoBehaviour {
 
 	// The SwitchController
 	public SwitchController theSwitch;
-
+    //this is a test for github
     private void Awake()
     {
         instance = this;
